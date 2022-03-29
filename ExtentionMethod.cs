f6fd297355cfd464dd65216c8c5539d742e9e442
Allahverdi1 <99805939+Allahverdi1@users.noreply.github.com>
2022-03-29 23:05:55 +0400

    Add files via upload

---
+++ ExtentionMethod.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task
+{
+    static internal class ExtentionMethod
+    {
+        public static bool IsOdd(this int num)
+        {
+            if (num % 2 == 0)
+                return false;
+            return true;
+        }
+
+        public static bool IsEven(this int num)
+        {
+            if (num % 2 == 0)
+                return true;
+            return false;
+        }
+        public static bool IsContainsDigit(this string str)
+        {
+            foreach (var item in str)
+            {
+                if (char.IsDigit(item))
+                    return true;
+            }
+            return false;
+        }
+
+        public static string ToCapitalize(this string str)
+        {
+            str = str.ToLower();
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            string newStr = char.ToUpper(str[0]) + str.Substring(1);
+            return newStr;
+        }
+
+        
+
+        public static int[] GetValueIndexes(this string str,char a)
+        {
+            int[] indexs = new int[0];
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(str[i] == a)
+                {
+                    Array.Resize(ref indexs, indexs.Length + 1);
+                    indexs[indexs.Length - 1] = i;
+                }
+            }
+            return indexs;
+        }
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    internal class Student
+    {
+        public string FullName { get; set; }
+        public string GroupNo { get; set; }
+        public int Age { get; set; }
+
+        public static bool CheckGroupNo(string str)
+        {
+            if(!string.IsNullOrWhiteSpace(str)&&str.Length==4)
+            {
+                if(char.IsUpper(str[0])&&char.IsDigit(str[1])&&char.IsDigit(str[2])&&char.IsDigit(str[3]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool CheckFullName(string str)
+        {
+            if(!string.IsNullOrWhiteSpace(str))
+            {
+                var word = str.Split(' ');
+
+                if(word.Length==2 && char.IsUpper(word[0][0]) && char.IsUpper(word[1][0]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
